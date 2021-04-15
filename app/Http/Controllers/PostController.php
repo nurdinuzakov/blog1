63fcd18614e7e2eb5fad3eb17f2541abcdff57da
Nurdin <nurdinuzakov@gmail.com>
2021-04-15 23:11:31 +0600

    created posts list

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class PostController extends Controller
+{
+    private $posts = [
+        1 => [
+            'id' => 1,
+            'title' => 'Bitcoin is 58000$',
+            'description' => 'Many text about bitcoin',
+            'imgUrl' => '#'
+        ],
+        2 => [
+            'id' => 2,
+            'title' => '2 Bitcoin is 58000$',
+            'description' => 'Many text about bitcoin',
+            'imgUrl' => '#'
+        ],
+        3 => [
+            'id' => 3,
+            'title' => '3 Bitcoin is 58000$',
+            'description' => 'Many text about bitcoin',
+            'imgUrl' => '#'
+        ],
+        4 => [
+            'id' => 4,
+            'title' => '4 Bitcoin is 58000$',
+            'description' => 'Many text about bitcoin',
+            'imgUrl' => '#'
+        ]
+    ];
+    public function posts()
+    {
+        return view('phones.list', [
+            'posts' => $this->posts
+        ]);
+    }
+}
